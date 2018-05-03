e39c206a8d1eceb3b14885cd44d1bdd7533ae09b
ms3236 <ms3236@cornell.edu>
2018-05-03 12:58:52 -0400

    init files

---
+++ app/accounts/models/song.py
@@ -0,0 +1,22 @@
+from . import *
+
+class Song(Base):
+  __tablename__ = 'songs'
+
+  name   = db.Column(db.String(128), nullable =False)
+  album  = db.Column(db.String(128))
+  artist = db.Column(db.ForeignKey('artists.id'), nullable =False)
+  uri    = db.Column(db.String(256), nullable =False)
+
+  def __init__(self, **kwargs):
+    self.name      = kwargs.get('name', None)
+    self.album     = kwargs.get('album', None)
+    self.artists   = kwargs.get('artist', None)
+    self.uri       = kwargs.get('uri', None)
+
+  def __repr__(self):
+    return str(self.__dict__)
+
+class SongSchema(ModelSchema):
+  class Meta:
+    model = Song
